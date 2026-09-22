20cb11f855199623ab2e42456ee260446093cd1b
bhutanairlinesth-cloud <bhutanairlinesth@gmail.com>
2026-09-22 18:59:57 +0700

    Bhutan-Center-V13.18.1-Hero-Image-Quality-HOTFIX-3-FILES

--- lib/site-images.ts
+++ lib/site-images.ts
@@ -1,17 +1,17 @@
 /**
  * Verified destination photography used on the public website.
- * Sources are Wikimedia Commons file pages for the named real-world places.
- * Keep captions/alt text aligned with the depicted location.
+ * Hotfix: upgrade hero images to high-resolution Wikimedia sources so
+ * destination pages do not appear blurry or pixelated on desktop.
  */
 export const siteImages = {
   tigerNest:
-    "/images/tiger-nest-paro-real.png",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Paro_Taktsang%2C_Bhutan_%28edited%29.jpg/1920px-Paro_Taktsang%2C_Bhutan_%28edited%29.jpg",
   punakhaDzong:
-    "https://upload.wikimedia.org/wikipedia/commons/5/58/Punakha_dzong.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Punakha_dzong.jpg/1920px-Punakha_dzong.jpg",
   thimphuDzong:
-    "https://upload.wikimedia.org/wikipedia/commons/c/c9/Tashichho_Dzong_2012-11-19.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Tashichho_Dzong_2012-11-19.jpg/1920px-Tashichho_Dzong_2012-11-19.jpg",
   phobjikhaValley:
-    "https://upload.wikimedia.org/wikipedia/commons/7/76/Phobjikha_valley.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Phobjikha_valley.jpg/1920px-Phobjikha_valley.jpg",
   bhutanAirlinesParo:
-    "https://upload.wikimedia.org/wikipedia/commons/3/3b/Bhutan_Airlines_Airbus_A319_%28A5-BAC%29_at_Paro_Airport.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Bhutan_Airlines_Airbus_A319_%28A5-BAC%29_at_Paro_Airport.jpg/1920px-Bhutan_Airlines_Airbus_A319_%28A5-BAC%29_at_Paro_Airport.jpg",
 } as const;
